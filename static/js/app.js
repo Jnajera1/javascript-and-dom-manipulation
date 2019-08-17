7b6ea8ab7da43b838598264ab76c2ed755f58e2b
Kathleen Graham <kathleeng77@outlook.com>
2019-08-17 15:41:24 -0500

    put data into table and added filter for user to choose date

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,47 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// create tbody variable to use when appending data
+var tbody = d3.select('tbody');
+
+// append objects from data.js into new table rows inside table body
+tableData.forEach(item => {
+    var tr = tbody.append('tr');
+    tr.append('td').text(item.datetime);
+    tr.append('td').text(item.city);
+    tr.append('td').text(item.state);
+    tr.append('td').text(item.country);
+    tr.append('td').text(item.shape);
+    tr.append('td').text(item.durationMinutes);
+    tr.append('td').text(item.comments);
+});
+
+// create button variable to be used in function
+var button = d3.select("#filter-btn");
+
+// function that will get user input from form and use it to filter upon clicking button
+button.on("click", function() {
+    // get the raw HTML from selection
+    var inputElement = d3.select("#datetime");
+
+    // get value property of the input element
+    var inputValue = inputElement.property("value");
+
+    // use input to filter data by date
+    var filtered = tableData.filter(item => item.datetime===inputValue);
+
+    // clear table info before appending filtered data
+    tbody.html(``);
+
+    // adding filtered data
+    filtered.forEach( item => {
+        var tr = tbody.append('tr');
+        tr.append('td').text(item.datetime);
+        tr.append('td').text(item.city);
+        tr.append('td').text(item.state);
+        tr.append('td').text(item.country);
+        tr.append('td').text(item.shape);
+        tr.append('td').text(item.durationMinutes);
+        tr.append('td').text(item.comments);
+    })
+});
