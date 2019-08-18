898f979fcd4ee8f947e62a3332782f95273a542e
Kathleen Graham <kathleeng77@outlook.com>
2019-08-17 19:40:35 -0500

    added state filter and intro to readme.md

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# javascript-and-dom-manipulation
-JavaScript and DOM Manipulation
+# JavaScript and DOM Manipulation
+
+### Data
+
+The data.js file had more entries such as these two:
+
+```javascript
+var data = [{
+    datetime: "1/1/2010",
+    city: "benton",
+    state: "ar",
+    country: "us",
+    shape: "circle",
+    durationMinutes: "5 mins.",
+    comments: "4 bright green circles high in the sky going in circles then one bright green light at my front door."
+  },
+  {
+    datetime: "1/1/2010",
+    city: "bonita",
+    state: "ca",
+    country: "us",
+    shape: "light",
+    durationMinutes: "13 minutes",
+    comments: "Three bright red lights witnessed floating stationary over San Diego New Years Day 2010"
+  }]
+```
+
--- static/js/app.js
+++ static/js/app.js
@@ -17,18 +17,20 @@ tableData.forEach(item => {
 });
 
 // create button variable to be used in function
-var button = d3.select("#filter-btn");
+var button = d3.select('#filter-btn');
 
 // function that will get user input from form and use it to filter upon clicking button
-button.on("click", function() {
+button.on('click', function() {
     // make variables for inputs and values for date and city
-    var dateInput = d3.select("#date-filter");
-    var dateValue = dateInput.property("value");
-    var cityInput = d3.select("#city-filter");
-    var cityValue = cityInput.property("value");
+    var dateInput = d3.select('#date-filter');
+    var dateValue = dateInput.property('value');
+    var cityInput = d3.select('#city-filter');
+    var cityValue = cityInput.property('value');
+    var stateInput = d3.select('#state-filter');
+    var stateValue = stateInput.property('value');
 
     // use input to filter data by date
-    var filtered = tableData.filter(item => item.datetime===dateValue && item.city===cityValue.toLowerCase());
+    var filtered = tableData.filter(item => item.datetime === dateValue && item.state === stateValue.toLowerCase() && item.city === cityValue.toLowerCase());
     // clear table info before appending filtered data
     tbody.html(``);
     // adding filtered data
@@ -43,5 +45,3 @@ button.on("click", function() {
         tr.append('td').text(item.comments);
     });
 });
-
-
